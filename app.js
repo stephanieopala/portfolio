7f5ee3c15e46d2d689d8a59546946ce9c4349f11
Stephanie Opala <rispahopala@gmail.com>
2021-07-09 19:33:35 +0300

    update navMenu functionality & add new project

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const toggleBtn = document.querySelector(".nav__togglebtn");
 const list = document.querySelector(".nav__list");
+const navItem = document.querySelectorAll(".nav__item");
 
 toggleBtn.addEventListener("click", () =>{
     list.classList.toggle ("active");
+});
+
+navItem.forEach(navMenu => {
+    navMenu.addEventListener("click", () => list.classList.toggle("active"))
 })
